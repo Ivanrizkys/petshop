105f1afb440f465d5b364fb5f4db683ff28cee31
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-11-24 09:49:07 +0700

    Initial

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Rambla:wght@700&display=swap"
+					rel="stylesheet"
+				/>
+				<title>Petshop</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="m-0 p-0 box-border">{children}</div>
+		</>
+	);
+};
+
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Container from "./Container";
+
+const Navbar = () => {
+    return (
+        <Container>
+            <nav>
+                <h1>Hallo Sayang</h1>
+            </nav>
+        </Container>
+    )
+}
+
+export default Navbar;
