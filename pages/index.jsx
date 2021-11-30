8b70285258f133b11b96c27d18e7c0aacd410d4f
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-11-30 21:26:12 +0700

    Adding IntroductionSection

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
 import Hero from "@/components/Hero";
+import IntroductionSection from "@/components/IntroductionSection";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+// import "swiper/css/pagination"
+// import "swiper/css/navigation"
+import SwiperCore, {Pagination,Navigation} from 'swiper';
+  
+// install Swiper modules
+SwiperCore.use([Pagination,Navigation]);
 
 export default function Home() {
   return (
     <>
       <Layout>
         <Hero/>
+        <IntroductionSection Swiper={Swiper} SwiperSlide={SwiperSlide}/>
       </Layout>
     </>
   )
