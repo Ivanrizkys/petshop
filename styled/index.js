e6fd24fedde5530ea5de4e203b9fd330fde6c998
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-11-30 21:33:24 +0700

    Create styled component for slider button in introduction section

---
+++ styled/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledButtonWraper = styled.div`
+    .swiper-button-disabled {
+        opacity: 0.5;
+        cursor: auto;   
+    }
+`
+
+export {StyledButtonWraper};
